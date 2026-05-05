f5cf986a7117d2782b5819149113f04965f24791
Liam Vuilleumier <liam.vuilleumier@epfl.ch>
2026-05-05 16:58:17 +0200

    first commit

---
+++ scanner.py
@@ -0,0 +1,33 @@
+import ast 
+import pathlib
+
+# This file is the crawler that looks through the project you want to analyze.
+# We want to look in the directory of the file and look only at the dependence of the files. 
+
+# find all the dependency in a .py file
+def findDependence(fileName):
+    with open(fileName, 'r') as f:
+        sourceCode = f.read()
+
+    imported_list = []
+    tree = ast.parse(sourceCode)
+
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Import):
+            for alias in node.names:
+                imported_list.append(alias.name)
+        if isinstance(node, ast.ImportFrom):
+            if node.module:
+                imported_list.append(node.module)
+    return imported_list
+
+
+# find all file in a directory
+#return List[File]
+def findFile(directoryName):
+    return list(pathlib.Path(directoryName).rglob("*.py"))
+
+
+fileNames = findFile(".")
+for i in fileNames:
+    print(findDependence(i))
